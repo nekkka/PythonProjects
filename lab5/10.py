9cbcbadc1b77635cb4362dd3f8490b0696f51aa7
Nelle <nurievaaa28@gmail.com>
2023-03-09 14:42:40 +0600

    init com

--- lab5/10.py
+++ lab5/10.py
@@ -3,13 +3,11 @@ import re
 
 txt = "asdAsd" # -> "asd_asd" camel to snake
 # txt = "asd_zxc" # -> "asdZxc" snake to camel
-x=re.findall("[A-Z]", txt)
+
+# x=re.findall("[A-Z]", txt)
+# print(x)
+
 s = ""
-print(x)
-
 s = re.sub(r"([A-Z])", r"_\1", txt)
-
-
-
 
 print(s.lower())
